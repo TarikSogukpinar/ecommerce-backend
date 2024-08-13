cbb524e090eb29e6fd943a4b24fc0f40319753c6
TarikSogukpinar <tariksogukpinar@outlook.com>
2024-08-13 13:22:34 +0300

    fix: deleted junk prisma column

---
+++ apps/nest-api/prisma/migrations/20240813102144_init/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `githubId` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `provider` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "githubId";
+ALTER TABLE "User" DROP COLUMN "provider";
