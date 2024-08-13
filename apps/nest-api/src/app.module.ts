ce24d01eb6a708adb0b8dd91a707fbc624139176
TarikSogukpinar <tariksogukpinar@outlook.com>
2024-08-13 12:05:53 +0300

    update:init nest.js dependencies

--- apps/nest-api/src/app.module.ts
+++ apps/nest-api/src/app.module.ts
@@ -1,10 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { PrismaModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: (() => {
+        const env = process.env.NODE_ENV;
+        const envFilePath =
+          env === 'production'
+            ? '.env.production'
+            : env === 'staging'
+              ? '.env.staging'
+              : '.env.development';
+        console.log(`Loading environment variables from ${envFilePath}`);
+        return envFilePath;
+      })(),
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    PrismaModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly configService: ConfigService) {
+    console.log(`NODE_ENV: ${this.configService.get('NODE_ENV')}`);
+  }
+}
--- apps/nest-api/src/main.ts
+++ apps/nest-api/src/main.ts
@@ -1,8 +1,51 @@
+//Custom Modules, Packages, Configs, etc.
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+//pnpm packages
+import helmet from 'helmet';
+import * as hpp from 'hpp';
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
+import validationOptions from './utils/validate/validation-options';
+import { SwaggerService } from './core/swagger/swagger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(6070);
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix(
+    configService.get<string>('API_GLOBAL_PREFIX', { infer: true }),
+  );
+  app.enableShutdownHooks();
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.use(helmet());
+  app.use(hpp());
+  app.use(compression());
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe(validationOptions));
+
+  const swaggerService = app.get(SwaggerService);
+  swaggerService.setupSwagger(app);
+  const PORT = configService.get<string>('API_PORT', { infer: true });
+
+  app.enableCors({
+    origin: [
+      configService.get<string>('CORS_ORIGIN', { infer: true }),
+      configService.get<string>('CORS_ORIGIN_LOCAL', { infer: true }),
+    ],
+    credentials: true,
+  });
+
+  await app.listen(
+    configService.get<number>('API_PORT', { infer: true }),
+    '0.0.0.0',
+  );
+
+  Logger.log(`🚀 Application is running on: http://localhost:${PORT}/`);
 }
 void bootstrap();
