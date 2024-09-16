41784eba9194364963cb3949dfd329beec9b823a
TarikSogukpinar <tariksogukpinar@outlook.com>
2024-09-16 10:05:45 +0300

    chore : maing.go clean up

--- apps/go-api/main.go
+++ apps/go-api/main.go
@@ -31,7 +31,6 @@ type Message struct {
 func main() {
 	// Load configuration and initialize environment variables
 	config.LoadConfig()
-	// jwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
 	// Create Fiber app instance
 	app := fiber.New()
@@ -135,49 +134,3 @@ func consumeMessages(ch *amqp.Channel) {
 
 	<-forever
 }
-
-// Consume messages from RabbitMQ
-// func consumeMessages(ch *amqp.Channel) {
-// 	msgs, err := ch.Consume(
-// 		"token_created_queue",
-// 		"",
-// 		true,  // Auto-acknowledge
-// 		false, // Exclusive
-// 		false, // No-local
-// 		false, // No-wait
-// 		nil,   // Arguments
-// 	)
-// 	if err != nil {
-// 		log.Fatalf("Queue messages could not be received: %v", err)
-// 	}
-
-// 	forever := make(chan bool)
-
-// 	go func() {
-// 		for d := range msgs {
-// 			log.Printf("Incoming message: %s", d.Body)
-
-// 			var payload TokenPayload
-// 			err := json.Unmarshal(d.Body, &payload)
-// 			if err != nil {
-// 				log.Printf("Message parsing error: %s", err)
-// 				continue
-// 			}
-
-// 			log.Printf("Message content: %v", payload)
-
-// 			log.Println("Access token...", payload.AccessToken)
-
-// 			claims, err := middleware.ValidateToken(payload.AccessToken)
-// 			if err != nil {
-// 				log.Println("Invalid token:", err)
-// 				continue
-// 			}
-
-// 			log.Printf("Token is valid, user: %v", claims["id"])
-
-// 		}
-// 	}()
-
-// 	<-forever
-// }
